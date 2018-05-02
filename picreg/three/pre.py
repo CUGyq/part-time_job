82b2018d8f025ccebd01d753a1701b9d430358eb
CUGyq <yangqing4497@qq.com>
2018-05-02 09:31:05 +0800

    图像识别

---
+++ picreg/three/pre.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import numpy as np
+import os
+import train
+import time
+import cv2
+MOVING_AVERAGE_DECAY = 0.99
+EVAL_INTERVAL_SECS = 10
+image_size = 128
+img = cv2.imread("./train/cat/cat.1.jpg")
+img0 = cv2.resize(img, (128, 128))
+img2 = tf.cast(img0,tf.float32)
+img3 = tf.reshape(img2,(1,128,128,3))
+y = train.inference(img3)
+qq = tf.nn.softmax(y)
+maxa = tf.argmax(y,1)
+q = qq[0][maxa[0]]
+variable_averages = tf.train.ExponentialMovingAverage(train.MOVING_AVERAGE_DECAY)
+variable_to_restore = variable_averages.variables_to_restore()
+saver = tf.train.Saver(variable_to_restore)
+with tf.Session() as sess:
+    tf.local_variables_initializer().run()
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+    ckpt = tf.train.get_checkpoint_state(train.MODEL_SAVE_PATH)
+    if ckpt and ckpt.model_checkpoint_path:
+        saver.restore(sess,ckpt.model_checkpoint_path)
+        global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+        for i in range(1):
+            ss= sess.run(maxa)
+            index = ""
+            if ss == 0:
+                index = "cat"
+            else:
+                index=  "dog"
+
+            cv2.putText(img, "class=%s"%index, (20, 20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
+            cv2.imshow("a", img)
+            cv2.waitKey(0)
+    else:
+        print("No checkpoint file found")
+    # time.sleep(EVAL_INTERVAL_SECS)
---
+++ picreg/two/input.py
@@ -0,0 +1,42 @@
+import numpy as np
+import os
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import cv2
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+def get_files(filename):
+    classes = []
+    class_train = []
+    label_train = []
+    for train_class in os.listdir(filename):
+        classes.append(train_class)
+
+    for index,name in enumerate(classes):
+        for pic in os.listdir(filename+name):
+            class_train.append(filename+name+'/'+pic)
+            label_train.append(index)
+    temp = np.array([class_train, label_train])
+    temp = temp.transpose()  #shuffle the samples
+    np.random.shuffle(temp)  #after transpose,
+    # images is in dimension 0 and label in dimension 1
+    image_list = list(temp[:, 0])
+    label_list = list(temp[:, 1])
+    writer = tf.python_io.TFRecordWriter("train.tfrecords")
+    for i in range(len(image_list)):
+        img = cv2.imread(image_list[i])
+        img = cv2.resize(img, (128, 128))
+        height, width, channels = img.shape[0], img.shape[1], img.shape[2]
+        img_raw = img.tostring()
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'height': _int64_feature(height),
+            'width': _int64_feature(width),
+            'channels': _int64_feature(channels),
+            'label': _int64_feature(int(label_list[i])),
+            'img_raw': _bytes_feature(img_raw)}))
+        writer.write(example.SerializeToString())
+    writer.close()
+if __name__ == '__main__':
+    get_files("./images/")
